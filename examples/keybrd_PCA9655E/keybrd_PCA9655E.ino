ae6b21b712187605c801176179e0f29ff88dfe70
wolfv6 <wolfv6@users.noreply.github.com>
2016-09-03 22:10:11 -0600

    add keybrd_PCA9655E.ino

--- examples/keybrd_PCA9655E/keybrd_PCA9655E.ino
+++ examples/keybrd_PCA9655E/keybrd_PCA9655E.ino
@@ -1,11 +1,7 @@
-/* keybrd_4c_split_with_IOE.ino
+/* keybrd_PCA9655E.ino
 
+  Teensy 2.0 controller     PCA9655E I/O expander
 | Left  | **0** | **1** | | Right | **0** | **1** |
-|:-----:|-------|-------| |:-----:|-------|-------|
-| **0** | a     | b     | | **0** | 1     | 2     |
-| **1** | shift | c     | | **1** | 3     | shift |
-
-| Left  | **0** | **1** | | Right | **0** | **1** | todo
 |:-----:|-------|-------| |:-----:|-------|-------|
 | **1** |   1   |   2   | | **1** |   3   |   4   |
 | **0** |   a   |   b   | | **0** |   c   |   d   |
@@ -21,21 +17,19 @@
 //right matrix
 #include <Row_IOE.h>
 #include <PortIOE.h>
-#include <PortWrite_MCP23S17.h>
-#include <PortRead_MCP23S17.h>
+#include <PortWrite_PCA9655E.h>
+#include <PortRead_PCA9655E.h>
 
 // ============ SPEED CONFIGURATION ============
 ScanDelay scanDelay(9000);
 
-// ================ LEFT MATRIX ================
-// ---------------- ACTIVE STATE ---------------
-const bool Scanner_uC::STROBE_ON = LOW;         //active low
-const bool Scanner_uC::STROBE_OFF = HIGH;
+// =============== LEFT uC MATRIX ==============
+const bool Scanner_uC::STROBE_ON = HIGH;       //active high
+const bool Scanner_uC::STROBE_OFF = LOW;
 
-// ------------------- PINS --------------------
-uint8_t readPins[] = {14, 15};
+uint8_t readPins_L[] = {0, 1};
 
-// ================ RIGHT MATRIX ===============
+// ============== RIGHT IOE MATRIX =============
 const bool Scanner_Port::STROBE_ON = HIGH;      //active high
 const bool Scanner_Port::STROBE_OFF = LOW;
 
@@ -43,41 +37,41 @@ const uint8_t PortIOE::DEVICE_ADDR = 0x18;
 
 // ------------------ PORT 1 -------------------
 PortIOE port1_R(1, 0);
-PortWrite_MCP23S17 portWrite1_R(port1_R);
+PortWrite_PCA9655E portWrite1_R(port1_R);
 
 // ------------------ PORT 0 -------------------
 PortIOE port0_R(0, 0);
-PortWrite_MCP23S17 portWrite0_R(port0_R);
-PortRead_MCP23S17 portRead0_R(port0_R, 1<<0 | 1<<1 );
+PortWrite_PCA9655E portWrite0_R(port0_R);
+PortRead_PCA9655E portRead0_R(port0_R, 1<<0 | 1<<1 );
 
 // =================== CODES ===================
-Code_Sc s_shiftL(MODIFIERKEY_LEFT_SHIFT);
-Code_Sc s_shiftR(MODIFIERKEY_RIGHT_SHIFT);
-
 Code_Sc s_a(KEY_A);
 Code_Sc s_b(KEY_B);
 Code_Sc s_c(KEY_C);
+Code_Sc s_d(KEY_D);
+
 Code_Sc s_1(KEY_1);
 Code_Sc s_2(KEY_2);
 Code_Sc s_3(KEY_3);
+Code_Sc s_4(KEY_4);
 
 // =================== ROWS ====================
 // ---------------- LEFT ROWS ------------------
-Key* ptrsKeys_L0[] = { &s_a, &s_b };
-const uint8_t KEY_COUNT_L0 = sizeof(ptrsKeys_L0)/sizeof(*ptrsKeys_L0);
-Row_uC row_L0(0, readPins, KEY_COUNT_L0, ptrsKeys_L0);
+Key* ptrsKeys_L0[] = { &s_1, &s_2 };
+uint8_t KEY_COUNT_L0 = sizeof(ptrsKeys_L0)/sizeof(*ptrsKeys_L0);
+Row_uC row_L0(21, readPins_L, KEY_COUNT_L0, ptrsKeys_L0);
 
-Key* ptrsKeys_L1[] = { &s_c,  &s_shiftL };
-const uint8_t KEY_COUNT_L1 = sizeof(ptrsKeys_L1)/sizeof(*ptrsKeys_L1);
-Row_uC row_L1(1, readPins, KEY_COUNT_L1, ptrsKeys_L1);
+Key* ptrsKeys_L1[] = { &s_a, &s_b };
+uint8_t KEY_COUNT_L1 = sizeof(ptrsKeys_L1)/sizeof(*ptrsKeys_L1);
+Row_uC row_L1(20, readPins_L, KEY_COUNT_L1, ptrsKeys_L1);
 
 // ---------------- RIGHT ROWS -----------------
-Key* ptrsKeys_R0[] = { &s_1, &s_2 };
-const uint8_t KEY_COUNT_R0 = sizeof(ptrsKeys_R0)/sizeof(*ptrsKeys_R0);
+Key* ptrsKeys_R0[] = { &s_3,  &s_4 };
+uint8_t KEY_COUNT_R0 = sizeof(ptrsKeys_R0)/sizeof(*ptrsKeys_R0);
 Row_IOE row_R0(portWrite1_R, 1<<0, portRead0_R, KEY_COUNT_R0, ptrsKeys_R0);
 
-Key* ptrsKeys_R1[] = { &s_3,  &s_shiftR };
-const uint8_t KEY_COUNT_R1 = sizeof(ptrsKeys_R1)/sizeof(*ptrsKeys_R1);
+Key* ptrsKeys_R1[] = { &s_c,  &s_d };
+uint8_t KEY_COUNT_R1 = sizeof(ptrsKeys_R1)/sizeof(*ptrsKeys_R1);
 Row_IOE row_R1(portWrite1_R, 1<<1, portRead0_R, KEY_COUNT_R1, ptrsKeys_R1);
 
 // ################### MAIN ####################
--- src/Row_IOE.h
+++ src/Row_IOE.h
@@ -7,15 +7,15 @@
 class PortWriteInterface;
 class PortReadInterface;
 
-/* Row_IOE is a row connected to an Input/Output Expander.
-Configuration and Instantiation instructions are in keybrd/src/Row_IOE.h
+/* Row_IOE is a row connected to an I/O Expander.
+Configuration and Instantiation instructions are in keybrd/src/Row_uC.h
 */
 class Row_IOE : public Row
 {
     private:
         Scanner_Port scanner;
         Debouncer_Samples debouncer;
-        const uint8_t readPinCount;             //number of read pins
+        const uint8_t readPinCount;
     public:
         Row_IOE(PortWriteInterface& refPortWrite, const uint8_t strobePin,
                   PortReadInterface& refPortRead, const uint8_t readPinCount, Key *const ptrsKeys[])
